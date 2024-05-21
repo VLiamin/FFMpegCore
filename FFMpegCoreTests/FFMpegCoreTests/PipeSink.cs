0c95c0274fef7543ec0aea3352bcbcb4d2ce49a3
v.liamin <lyaminva@yandex.ru>
2024-05-22 00:58:27 +0300

    Add ffmpegcore project

---
+++ FFMpegCoreTests/FFMpegCoreTests/PipeSink.cs
@@ -0,0 +1,22 @@
+﻿using FFMpegCore.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFMpegCoreTests
+{
+	public class PipeSink : IPipeSink
+	{
+		public string GetFormat()
+		{
+			return string.Empty;
+		}
+
+		public Task ReadAsync(Stream inputStream, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ FFMpegCoreTests/FFMpegCoreTests/Program.cs
@@ -0,0 +1,40 @@
+﻿using FFMpegCore.Pipes;
+using FFMpegCore;
+using System.Reflection;
+using FFMpegCore.Enums;
+using System.Security.Cryptography;
+
+namespace FFMpegCoreTests
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Program program = new Program();
+			program.Convert();
+		}
+
+		public async void Convert()
+		{
+			string FilePath = "audio1.mp3";
+			string FilePath2 = "audio2.mp4";
+			Stream outputStream = new MemoryStream();
+
+			var arg = FFMpegArguments
+	.FromFileInput(FilePath)
+	.OutputToFile(FilePath2, true, options => options
+		.WithVideoCodec(VideoCodec.LibX264)
+		.WithConstantRateFactor(21)
+		.WithAudioCodec(AudioCodec.Aac)
+		.WithVariableBitrate(4)
+		.WithVideoFilters(filterOptions => filterOptions.Scale(VideoSize.FullHd))
+		.WithFastStart())
+	.ProcessSynchronously();
+			/*			var mysink = new PipeSink();
+						FFMpegArguments.FromFileInput(FilePath)
+							.OutputToPipe(mysink).ProcessSynchronously();*/
+
+			var t = 1;
+		}
+	}
+}
